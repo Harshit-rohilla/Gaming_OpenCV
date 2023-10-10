68e0cb460184858dad7731681e069ec57cb6a52e
Harshit-rohilla <harshitrohilla105@gmail.com>
2023-10-10 23:47:53 +0530

    fixed

--- Hand_Detector.py
+++ Hand_Detector.py
@@ -2,19 +2,18 @@ import cv2
 import mediapipe as mp
 import os
 class handDetector():
-    def __init__(self,mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self,mode=False, maxHands=2, detectionCon=0.5, trackCon=0.75):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-
+        # self.modelComplex = modelComplexity
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.detectionCon, self.trackCon)
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,int(self.detectionCon), self.trackCon)
         self.mpDraw = mp.solutions.drawing_utils
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
---
+++ run.py
@@ -0,0 +1,2 @@
+import DriverFunction
+DriverFunction.Game()
